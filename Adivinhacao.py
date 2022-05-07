df17cba74d3355acaf6d9d6e203822f4f94f8dfd
Willian <willian_assufialbanez@hotmail.com>
2022-05-06 22:59:43 -0300

    Pausa

--- Adivinhacao.py
+++ Adivinhacao.py
@@ -1,1 +1,24 @@
+print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
+print("*********************************")
+
+numero_secreto = 42
+tentativas = 3
+contador = 1
+
+while(contador <= tentativas):
+    print("Tentativa {} de {}".format(contador, tentativas))
+    chute_str = input("Digite o seu número: ")
+    print("Voce digitou ", chute_str)
+    chute = int(chute_str)
+
+    if(numero_secreto == chute):
+        print("Voce acertou")
+    elif(chute > numero_secreto):
+        print("Voce errou! O seu chute foi maior que o numero secreto")
+    else:
+        print("Voce errou! O seu chute foi menor que o numero secreto")
+
+    contador += 1
+
+print("Fim do jogo")
