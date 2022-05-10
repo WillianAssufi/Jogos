b0dd8b9a9dfcc4c7380f3b0707c8eab6933853b1
Willian <willian_assufialbanez@hotmail.com>
2022-05-10 09:49:34 -0300

    Adicionado quebra de linha

--- Adivinhacao.py
+++ Adivinhacao.py
@@ -8,11 +8,11 @@ numero_secreto = random.randrange(1, 101)
 tentativas = 0
 pontos = 1000
 
-print("Qual o nível de dificuldade?")
+print("\nQual o nível de dificuldade?")
 print("(1) Fácil \n(2) Médio\n(3) Difícil")
 
 nivel = int(input("Defina o nível: "))
-
+print()
 if(nivel == 1):
     tentativas = 20
 elif(nivel == 2):
@@ -28,16 +28,16 @@ for contador in range(1, tentativas + 1):
     chute = int(chute_str)
 
     if(chute < 1 or chute > 100):
-        print("Voce deve digitar um número entre 1 e 100!")
+        print("Voce deve digitar um número entre 1 e 100!\n")
         continue
 
     if(numero_secreto == chute):
-        print("Voce acertou e fez {} pontos!".format(pontos))
+        print("\nVoce acertou e fez {} pontos!\n".format(pontos))
         break
     elif(chute > numero_secreto):
-        print("Voce errou! O seu chute foi maior que o numero secreto")
+        print("Voce errou! O seu chute foi maior que o numero secreto\n")
     else:
-        print("Voce errou! O seu chute foi menor que o numero secreto")
+        print("Voce errou! O seu chute foi menor que o numero secreto\n")
 
     if(chute != numero_secreto):
         pontos_perdidos = abs(numero_secreto - chute)
