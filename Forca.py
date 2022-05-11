01ffdc94e9e6aac51e98ae7d4f4d4fd4a524c259
Willian <willian_assufialbanez@hotmail.com>
2022-05-11 17:44:57 -0300

    Implementando Jogo da Forca

--- Forca.py
+++ Forca.py
@@ -4,6 +4,16 @@ def jogar():
     print("***Bem vindo ao jogo de Forca!***")
     print("*********************************")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+
+        chute = input("Escolha uma letra")
+
+        print("Jogando ...")
+
     print("Fim de jogo!")
 
 if(__name__ == "__main__"):
