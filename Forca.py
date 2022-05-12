700022038638e5993eeb09ea941acb75efd641a3
Willian <willian_assufialbanez@hotmail.com>
2022-05-12 19:47:00 -0300

    Tratando uma string e buscando letras

--- Forca.py
+++ Forca.py
@@ -9,8 +9,16 @@ def jogar():
     acertou = False
 
     while(not enforcou and not acertou):
-
-        chute = input("Escolha uma letra")
+        
+        chute = input("Escolha uma letra: ")
+        chute = chute.strip()
+        
+        index = 0
+        
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index += 1
 
         print("Jogando ...")
 
