08e3f880e9ffb1a694ab7e1f271205959cda8470
Willian <willian_assufialbanez@hotmail.com>
2022-05-15 12:27:10 -0300

    Criando lista de frutas

--- Forca.py
+++ Forca.py
@@ -4,27 +4,38 @@ def jogar():
     print("***Bem vindo ao jogo de Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_","_","_","_","_","_"]
+    palavra_secreta = "maça".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
     
 
     enforcou = False
     acertou = False
+    erros = 0
+
+    print(letras_acertadas)
 
     while(not enforcou and not acertou):
         
         chute = input("Escolha uma letra: ")
-        chute = chute.strip()
-        
-        index = 0
-        
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-            index += 1
+        chute = chute.strip().upper()
 
+        if(chute in palavra_secreta):
+            index = 0
+            
+            for letra in palavra_secreta:
+                if(chute.upper() == letra.upper()):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+        enforcou = erros == len(palavra_secreta)
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
 
+    if(acertou):
+        print("Voce ganhou!")
+    else:
+        print("Voce perdeu!")
     print("Fim de jogo!")
 
 if(__name__ == "__main__"):
